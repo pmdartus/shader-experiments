253604ef0af9dbffa969240b13e536ec1cffe055
Pierre-Marie Dartus <pm@dartus.fr>
2020-07-21 06:04:27 +0200

    Add controls

--- shader-preview/src/Preview.tsx
+++ shader-preview/src/Preview.tsx
@@ -1,57 +1,57 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
-import { createShader, createProgram } from './webgl/shader';
-import './Preview.css';
+import { createShader, createProgram } from "./webgl/shader";
+import "./Preview.css";
 
 enum ColorChannel {
-    RGB = 'RGB',
-    R = 'R',
-    G = 'G',
-    B = 'B',
+  RGB = "RGB",
+  R = "R",
+  G = "G",
+  B = "B",
 }
 
 const ZOOM_STEP = 0.2;
 
 const DISPLAY_CHANNELS: Record<
-    ColorChannel,
-    { label: string; filter: number[] }
+  ColorChannel,
+  { label: string; filter: number[] }
 > = {
-    RGB: {
-        label: 'RBG',
-        // prettier-ignore
-        filter: [
+  RGB: {
+    label: "RBG",
+    // prettier-ignore
+    filter: [
       1, 0, 0,
       0, 1, 0,
       0, 0, 1
     ]
-    },
-    R: {
-        label: 'Red',
-        // prettier-ignore
-        filter: [
+  },
+  R: {
+    label: "Red",
+    // prettier-ignore
+    filter: [
       1, 0, 0,
       1, 0, 0,
       1, 0, 0,
     ]
-    },
-    G: {
-        label: 'Green',
-        // prettier-ignore
-        filter: [
+  },
+  G: {
+    label: "Green",
+    // prettier-ignore
+    filter: [
       0, 1, 0,
       0, 1, 0,
       0, 1, 0
     ]
-    },
-    B: {
-        label: 'Blue',
-        // prettier-ignore
-        filter: [
+  },
+  B: {
+    label: "Blue",
+    // prettier-ignore
+    filter: [
       0, 0, 1,
       0, 0, 1,
       0, 0, 1
     ]
-    },
+  },
 };
 
 // prettier-ignore
@@ -94,13 +94,14 @@ precision highp float;
 uniform sampler2D u_image;
 uniform mat3 u_channelsFilter;
 uniform bool u_tiling;
+uniform float u_zoom;
 
 in vec2 v_texCoord;
 
 out vec4 outColor;
 
 void main() {
-  vec2 pos = v_texCoord * vec2(1.2);
+  vec2 pos = v_texCoord * vec2(u_zoom);
 
   if (!u_tiling && any(greaterThan(abs(pos), vec2(1.0)))) {
     discard;
@@ -111,89 +112,89 @@ void main() {
 `;
 
 function loadImageData(url: string): Promise<ImageData> {
-    const image = new Image();
-    image.crossOrigin = 'anonymous';
+  const image = new Image();
+  image.crossOrigin = "anonymous";
 
-    return new Promise((resolve, reject) => {
-        image.onload = () => {
-            const canvas = document.createElement('canvas');
-            canvas.width = image.width;
-            canvas.height = image.height;
+  return new Promise((resolve, reject) => {
+    image.onload = () => {
+      const canvas = document.createElement("canvas");
+      canvas.width = image.width;
+      canvas.height = image.height;
 
-            const ctx = canvas.getContext('2d');
+      const ctx = canvas.getContext("2d");
 
-            if (ctx === null) {
-                return reject(new Error(`Can't access 2d context`));
-            }
+      if (ctx === null) {
+        return reject(new Error(`Can't access 2d context`));
+      }
 
-            ctx.drawImage(image, 0, 0);
-            const data = ctx.getImageData(0, 0, image.width, image.height);
+      ctx.drawImage(image, 0, 0);
+      const data = ctx.getImageData(0, 0, image.width, image.height);
 
-            resolve(data);
-        };
+      resolve(data);
+    };
 
-        image.src = url;
-    });
+    image.src = url;
+  });
 }
 
 function drawPreview(
-    canvas: HTMLCanvasElement,
-    config: { imageData: ImageData; channels: ColorChannel; tiling: boolean },
+  canvas: HTMLCanvasElement,
+  config: {
+    imageData: ImageData;
+    channels: ColorChannel;
+    tiling: boolean;
+    zoom: number;
+  }
 ): void {
-    const { imageData, channels, tiling } = config;
-    const { clientWidth, clientHeight } = canvas;
+  const { imageData, channels, tiling, zoom } = config;
+  const { clientWidth, clientHeight } = canvas;
 
-    if (clientWidth !== canvas.width || clientHeight !== canvas.height) {
-        canvas.width = clientWidth;
-        canvas.height = clientHeight;
-    }
+  if (clientWidth !== canvas.width || clientHeight !== canvas.height) {
+    canvas.width = clientWidth;
+    canvas.height = clientHeight;
+  }
 
-    const gl = canvas.getContext('webgl2');
+  const gl = canvas.getContext("webgl2");
 
-    if (!gl) {
-        throw new Error(`Can't access webgl2 context`);
-    }
+  if (!gl) {
+    throw new Error(`Can't access webgl2 context`);
+  }
 
-    const vertexShader = createShader(gl, gl.VERTEX_SHADER, VERTEX_SHADER_SRC);
-    const fragmentShader = createShader(
-        gl,
-        gl.FRAGMENT_SHADER,
-        FRAGMENT_SHADER_SRC,
-    );
-    const program = createProgram(gl, vertexShader, fragmentShader);
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, VERTEX_SHADER_SRC);
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    FRAGMENT_SHADER_SRC
+  );
+  const program = createProgram(gl, vertexShader, fragmentShader);
 
-    const positionAttributeLocation = gl.getAttribLocation(
-        program,
-        'a_position',
-    );
-    const texCoordAtributeLocation = gl.getAttribLocation(
-        program,
-        'a_texCoord',
-    );
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const texCoordAtributeLocation = gl.getAttribLocation(program, "a_texCoord");
 
-    const resolutionUniformLocation = gl.getUniformLocation(
-        program,
-        'u_resolution',
-    );
-    const imageUniformLocation = gl.getUniformLocation(program, 'u_image');
-    const channelsFilerLocation = gl.getUniformLocation(
-        program,
-        'u_channelsFilter',
-    );
-    const tilingUniformLocation = gl.getUniformLocation(program, 'u_tiling');
+  const resolutionUniformLocation = gl.getUniformLocation(
+    program,
+    "u_resolution"
+  );
+  const imageUniformLocation = gl.getUniformLocation(program, "u_image");
+  const channelsFilerLocation = gl.getUniformLocation(
+    program,
+    "u_channelsFilter"
+  );
+  const tilingUniformLocation = gl.getUniformLocation(program, "u_tiling");
+  const zoomUnfiormLocation = gl.getUniformLocation(program, "u_zoom");
 
-    const vao = gl.createVertexArray();
-    gl.bindVertexArray(vao);
+  const vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
 
-    const positionBuffer = gl.createBuffer();
-    gl.enableVertexAttribArray(positionAttributeLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  const positionBuffer = gl.createBuffer();
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    const textCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, textCoordBuffer);
-    // prettier-ignore
-    gl.bufferData(
+  const textCoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textCoordBuffer);
+  // prettier-ignore
+  gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([
       0, 0, 
@@ -206,112 +207,157 @@ function drawPreview(
     gl.STATIC_DRAW
   );
 
-    gl.enableVertexAttribArray(texCoordAtributeLocation);
-    gl.vertexAttribPointer(texCoordAtributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(texCoordAtributeLocation);
+  gl.vertexAttribPointer(texCoordAtributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    const texture = gl.createTexture();
-    gl.activeTexture(gl.TEXTURE0 + 0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
+  const texture = gl.createTexture();
+  gl.activeTexture(gl.TEXTURE0 + 0);
+  gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-    gl.texImage2D(
-        gl.TEXTURE_2D,
-        0,
-        gl.RGBA,
-        gl.RGBA,
-        gl.UNSIGNED_BYTE,
-        imageData,
-    );
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGBA,
+    gl.RGBA,
+    gl.UNSIGNED_BYTE,
+    imageData
+  );
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.viewport(0, 0, canvas.width, canvas.height);
 
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    gl.useProgram(program);
+  gl.useProgram(program);
 
-    gl.bindVertexArray(vao);
+  gl.bindVertexArray(vao);
 
-    gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
-    gl.uniform1i(imageUniformLocation, 0);
-    gl.uniformMatrix3fv(
-        channelsFilerLocation,
-        false,
-        DISPLAY_CHANNELS[channels].filter,
-    );
-    gl.uniform1i(tilingUniformLocation, tiling ? 1 : 0);
+  gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
+  gl.uniform1i(imageUniformLocation, 0);
+  gl.uniformMatrix3fv(
+    channelsFilerLocation,
+    false,
+    DISPLAY_CHANNELS[channels].filter
+  );
+  gl.uniform1i(tilingUniformLocation, tiling ? 1 : 0);
+  gl.uniform1f(zoomUnfiormLocation, zoom);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, POSITION_VERTEX, gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, POSITION_VERTEX, gl.STATIC_DRAW);
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
 function Preview(props: { url: string }) {
-    const [imageData, setImageData] = useState<null | ImageData>(null);
-    const [channels, setChannels] = useState<ColorChannel>(ColorChannel.RGB);
-    const [tiling, setTiling] = useState<boolean>(false);
-    const [zoom, setZoom] = useState<number>(1);
+  const [imageData, setImageData] = useState<null | ImageData>(null);
+  const [channels, setChannels] = useState<ColorChannel>(ColorChannel.RGB);
+  const [tiling, setTiling] = useState<boolean>(false);
+  const [zoom, setZoom] = useState<number>(1);
 
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    useEffect(() => {
-        loadImageData(props.url).then((imageData) => {
-            setImageData(imageData);
-        });
-    }, [props.url]);
+  useEffect(() => {
+    loadImageData(props.url).then((imageData) => {
+      setImageData(imageData);
+    });
+  }, [props.url]);
 
-    useEffect(() => {
-        if (canvasRef.current && imageData) {
-            drawPreview(canvasRef.current, {
-                imageData,
-                channels,
-                tiling,
-            });
-        }
-    }, [imageData, channels, tiling]);
+  useEffect(() => {
+    if (canvasRef.current && imageData) {
+      drawPreview(canvasRef.current, {
+        imageData,
+        channels,
+        tiling,
+        zoom,
+      });
+    }
+  }, [imageData, channels, tiling, zoom]);
 
-    const handleZoomIncrease = () => {
-      setZoom(zoom + zoom * ZOOM_STEP);
-    };
+  console.log(zoom);
 
-    const handleZoomDecrease = () => {
-      setZoom(zoom - zoom * ZOOM_STEP);
-    };
+  return (
+    <div className="preview">
+      <ColorChannelControl value={channels} onChange={(value) => setChannels(value)}/>
 
-    return (
-        <div className="preview">
-            <select
-                onChange={(e) => setChannels(e.target.value as ColorChannel)}
-            >
-                {Object.entries(DISPLAY_CHANNELS).map(([name, { label }]) => (
-                    <option key={name} value={name}>
-                        {label}
-                    </option>
-                ))}
-            </select>
+      <label htmlFor="tiling">Tiling</label>
+      <input
+        type="checkbox"
+        id="tiling"
+        name="tiling"
+        title={tiling ? "Disable tiling" : "Enable tiling"}
+        checked={tiling}
+        onChange={(e) => setTiling(e.target.checked)}
+      />
 
-            <label htmlFor="tiling">Tiling</label>
-            <input
-                type="checkbox"
-                id="tiling"
-                name="tiling"
-                title={tiling ? "Disable tiling" : "Enable tiling"}
-                checked={tiling}
-                onChange={(e) => setTiling(e.target.checked)}
-            />
+      <ZoomControl value={zoom} onChange={(value) => setZoom(value)} />
 
-            <button title="Decrease zoom" onClick={handleZoomDecrease}>-</button>
-            <input type="text" name="zoom" id="zoom" value={zoom}/>
-            <button title="Increase zoom" onClick={handleZoomIncrease}>+</button>
+      <hr />
 
-            <hr />
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+}
 
-            <canvas ref={canvasRef}></canvas>
-        </div>
-    );
+function ColorChannelControl(props: {
+  value: ColorChannel;
+  onChange: (value: ColorChannel) => void;
+}) {
+  const { value, onChange } = props;
+
+  return (
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value as ColorChannel)}
+    >
+      {Object.entries(DISPLAY_CHANNELS).map(([name, { label }]) => (
+        <option key={name} value={name}>
+          {label}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function ZoomControl(props: {
+  value: number;
+  onChange: (value: number) => void;
+}) {
+  const { value, onChange } = props;
+
+  const handleZoomDecreaseClick = () => {
+    onChange(value / (1 + ZOOM_STEP));
+  };
+
+  const handleZoomIncreaseClick = () => {
+    onChange(value * (1 + ZOOM_STEP));
+  };
+
+  const handleZoomValueChange = (newValue: number) => {
+    onChange(newValue / 100);
+  };
+
+  return (
+    <div className="zoom-control">
+      <button title="Decrease zoom" onClick={handleZoomDecreaseClick}>
+        -
+      </button>
+
+      <input
+        type="number"
+        name="zoom"
+        id="zoom"
+        value={(value * 100).toFixed(2)}
+        onChange={(e) => handleZoomValueChange(e.target.valueAsNumber)}
+      />
+
+      <button title="Increase zoom" onClick={handleZoomIncreaseClick}>
+        +
+      </button>
+    </div>
+  );
 }
 
 export default Preview;
