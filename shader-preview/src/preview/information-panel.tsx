f4c6e33da2fd16803b1061c02df9e3cb5dda3927
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-07-21 18:28:23 +0200

    Add color tests

--- shader-preview/src/preview/information-panel.tsx
+++ shader-preview/src/preview/information-panel.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import { Flex, View, Heading, Divider } from '@adobe/react-spectrum';
+import { Flex, View, Heading, Divider } from "@adobe/react-spectrum";
 
-import { ColorRgba, rgbaToHex, rgbaToHsl } from '../shared/color';
+import { ColorRgba, rgbaToHex, rgbaToHsl } from "../shared/color";
 
 const UNIT8_MAX = 255;
 const BLACK_COLOR: ColorRgba = [0, 0, 0, 0];
 
-function getPixelAt(imageData: ImageData, position: [number, number]): ColorRgba {
+function getPixelAt(
+  imageData: ImageData,
+  position: [number, number]
+): ColorRgba {
   const { data, width } = imageData;
-  const offset = (position[1] * (width * 4)) + (position[0] * 4);
-  
-  return offset < data.length ? 
-    [data[offset], data[offset + 1], data[offset + 2], data[offset + 3]] :
-    BLACK_COLOR;
+  const offset = position[1] * (width * 4) + position[0] * 4;
+
+  return offset < data.length
+    ? [data[offset], data[offset + 1], data[offset + 2], data[offset + 3]]
+    : BLACK_COLOR;
 }
 
 function getPixelData(position: [number, number], imageData: ImageData | null) {
@@ -31,54 +34,60 @@ function getPixelData(position: [number, number], imageData: ImageData | null) {
         a: `A: ${rgba[3]} / ${(rgba[3] / UNIT8_MAX).toFixed(3)}`,
       },
       hsv: {
-        h: `H: ${Math.floor(hsl[0])} / ${(hsl[0] / UNIT8_MAX).toFixed(3)}`,
-        s: `S: ${Math.floor(hsl[1])} / ${(hsl[1] / UNIT8_MAX).toFixed(3)}`,
-        l: `L: ${Math.floor(hsl[2])} / ${(hsl[2] / UNIT8_MAX).toFixed(3)}`,
-        hex: `Hex: ${hex}`
+        h: `H: ${Math.floor(hsl[0])} / ${(hsl[0] / 360).toFixed(3)}`,
+        s: `S: ${Math.floor(hsl[1])} / ${(hsl[1] / 100).toFixed(3)}`,
+        l: `L: ${Math.floor(hsl[2])} / ${(hsl[2] / 100).toFixed(3)}`,
+        hex: `Hex: ${hex}`,
       },
       position: {
-        x: imageData ? `X: ${position[0]} / ${(position[0] / imageData.width).toFixed(2)}` : `X: 0 / 0`,
-        y: imageData ? `X: ${position[1]} / ${(position[1] / imageData.height).toFixed(2)}` : `Y: 0 / 0`,
+        x: imageData
+          ? `X: ${position[0]} / ${(position[0] / imageData.width).toFixed(2)}`
+          : `X: 0 / 0`,
+        y: imageData
+          ? `X: ${position[1]} / ${(position[1] / imageData.height).toFixed(2)}`
+          : `Y: 0 / 0`,
       },
-    }
+    },
   };
 }
 
 export function InformationPanel(props: {
-    position: [number, number];
-    imageData: ImageData | null;
-  }) {
-    const { position, imageData } = props;
+  position: [number, number];
+  imageData: ImageData | null;
+}) {
+  const { position, imageData } = props;
 
-    const { color, info } = getPixelData(position, imageData);
-  
-    const informationSection = Object.entries(info).map(([name, col]) => (
-      <Flex key={name} direction="column">
-        {Object.entries(col).map(([name, row]) => (
-          <View key={name}>{row}</View>
-        ))}
-      </Flex>
-    ));
-  
-    return (
-      <div>
-        <Heading>Information</Heading>
-        <Flex direction="row" gap="size-200">
-          <View
-            width="size-200"
-            height="size-200"
-            UNSAFE_style={{
-              backgroundColor: `rgba(${color.join(", ")})`,
-            }}
-          />
-  
-          <Divider orientation="vertical" />
-  
-          <Flex direction="row" gap="size-200" flex="1" justifyContent="space-between">
-            {informationSection}
-          </Flex>
+  const { color, info } = getPixelData(position, imageData);
+
+  return (
+    <div>
+      <Heading>Information</Heading>
+      <Flex direction="row" gap="size-200">
+        <View
+          width="size-200"
+          height="size-200"
+          UNSAFE_style={{
+            backgroundColor: `rgba(${color.join(", ")})`,
+          }}
+        />
+
+        <Divider orientation="vertical" />
+
+        <Flex
+          direction="row"
+          gap="size-200"
+          flex="1"
+          justifyContent="space-between"
+        >
+          {Object.entries(info).map(([name, col]) => (
+            <Flex key={name} direction="column">
+              {Object.entries(col).map(([name, row]) => (
+                <View key={name}>{row}</View>
+              ))}
+            </Flex>
+          ))}
         </Flex>
-      </div>
-    );
-  }
-  
+      </Flex>
+    </div>
+  );
+}
---
+++ shader-preview/src/shared/color.test.ts
@@ -0,0 +1,39 @@
+import { rgbaToHex, rgbaToHsl, ColorRgba, ColorHex, ColorHsl } from './color';
+
+function testRgbaToHex(rgba: ColorRgba, expected: ColorHex) {
+    test(`rgbaToHex(${JSON.stringify(rgba)})`, () => {
+        expect(rgbaToHex(rgba)).toBe(expected);
+    });
+}
+
+function testRgbaToHsl(rgba: ColorRgba, expected: ColorHsl) {
+    test(`rgbaToHex(${JSON.stringify(rgba)})`, () => {
+        const actual = rgbaToHsl(rgba);
+
+        expect(actual[0]).toBeCloseTo(expected[0]);
+        expect(actual[1]).toBeCloseTo(expected[1]);
+        expect(actual[2]).toBeCloseTo(expected[2]);
+    });
+}
+
+describe('rgbaToHex', () => {
+    testRgbaToHex([0, 0, 0, 0], '#000000');
+    testRgbaToHex([255, 255, 255, 255], '#ffffff');
+
+    testRgbaToHex([255, 0, 0, 0], '#ff0000');
+    testRgbaToHex([0, 255, 0, 0], '#00ff00');
+    testRgbaToHex([0, 0, 255, 0], '#0000ff');
+
+    testRgbaToHex([17, 85, 204, 0], '#1155cc');
+});
+
+describe('rgbaToHex', () => {
+    testRgbaToHsl([0, 0, 0, 0], [0, 0, 0]);
+    testRgbaToHsl([255, 255, 255, 255], [0, 0, 100]);
+
+    testRgbaToHsl([255, 0, 0, 0], [0, 100, 50]);
+    testRgbaToHsl([0, 255, 0, 0], [120, 100, 50]);
+    testRgbaToHsl([0, 0, 255, 0], [240, 100, 50]);
+
+    testRgbaToHsl([17, 85, 204, 0], [218.185, 84.615, 43.333]);
+});
